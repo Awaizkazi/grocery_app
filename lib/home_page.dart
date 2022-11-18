011760763d3faea4fab4c285ece22c81074c867e
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-18 23:45:55 +0530

    c

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'components/grocery_item_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,10 +9,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Home Page'),
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 47),
+            // Good morning
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text('Good Morning,'),
+            ),
+            const SizedBox(height: 4),
+            // Let's order fresh items for you
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                'Let\'s order fresh items for you',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.notoSerif(
+                    fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 24),
+            // Divider
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Divider(thickness: 4),
+            ),
+            // Fresh items + grid
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                'Fresh Items',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            // Gird view for items
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: ((context, index) {
+                  return GroceryItemTile(); 
+                }),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
