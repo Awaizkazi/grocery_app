02bdd5448ef66f5a9a39b5824816e10037f97e74
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-19 15:29:13 +0530

    c

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -19,9 +19,26 @@ class CartPage extends StatelessWidget {
               Expanded(
                 child: ListView.builder(
                   itemCount: value.cartItems.length,
+                  padding: EdgeInsets.all(12),
                   itemBuilder: ((context, index) {
-                    return ListTile(
-                      title: Text(value.cartItems[index][0]),
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.grey[200]),
+                        child: ListTile(
+                          leading: Image.asset(value.cartItems[index][2],
+                              height: 30),
+                          title: Text(value.cartItems[index][0]),
+                          subtitle: Text('\$' + value.cartItems[index][1]),
+                          trailing: IconButton(
+                            onPressed: () {
+                              Provider.of<Cartmodel>(context, listen: false)
+                                  .removeItemFromCart(index);
+                            },
+                            icon: const Icon(Icons.cancel),
+                          ),
+                        ),
+                      ),
                     );
                   }),
                 ),
