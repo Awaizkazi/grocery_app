fc9a9b4d6200feb366b061b63cf283ec34d2105c
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-19 14:20:21 +0530

    Calculate Total

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -19,15 +19,22 @@ class Cartmodel extends ChangeNotifier {
   // Add item to cart
   void addItemToCart(int index) {
     _cartItems.add(_shopItems[index]);
-    notifyListeners(); // To Notify the listener to help all the other widgets to update the data
+    notifyListeners(); // To Notify the listener to help all the other widgets to update the data  Data Add kar diya hai cart me toh add karke update kardo
   }
 
   // Remove item from cart
   void removeItemFromCart(int index) {
     _cartItems.removeAt(index);
-    notifyListeners(); //  items remove kar diya hai ab Update  kardo Isliye notifyListener use kiya hai
+    notifyListeners(); //  items remove kar diya hai remove karke  Update  kardo Isliye notifyListener use kiya hai
   }
 
   // Calaulate total price
-
+  String calculateTotal() {
+    double totalPrice = 0.0;
+    for (int i = 0; i < _cartItems.length; i++) {
+      totalPrice +=
+          double.parse(_cartItems[i][1]); //  index 1 is for the itemPrice
+    }
+    return totalPrice.toStringAsFixed(2);
+  }
 }
