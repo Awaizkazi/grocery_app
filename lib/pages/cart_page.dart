4ed719c97a7912a5de6cbe940ad8707cb1db4510
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-19 15:09:45 +0530

    c

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:grocery_app/model/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -12,18 +12,23 @@ class CartPage extends StatelessWidget {
         title: const Text('My Cart'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: ((context, index) {
-                return ListTile(
-                  title: Text('ietms'),
-                );
-              }),
-            ),
-          ),
-        ],
+      body: Consumer<Cartmodel>(
+        builder: ((context, value, child) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.cartItems.length,
+                  itemBuilder: ((context, index) {
+                    return ListTile(
+                      title: Text(value.cartItems[index][0]),
+                    );
+                  }),
+                ),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
